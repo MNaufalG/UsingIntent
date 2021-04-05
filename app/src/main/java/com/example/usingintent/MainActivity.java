6915b246e0dbcbccc5e44ad697803ee2c7074cae
MNaufalG <mochamad.naufal.tif19@polban.ac.id>
2021-04-05 14:21:06 +0700

    UsingIntent

---
+++ app/src/main/java/com/example/usingintent/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.usingintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        startActivity(new Intent("com.example.usingintent.SecondActivity"));
+    }
+}
